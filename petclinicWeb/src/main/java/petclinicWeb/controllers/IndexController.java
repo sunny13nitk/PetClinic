4ef6f9525eaa335bca26b1080d8c36ed3f5d65bf
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2020-10-02 13:10:58 +0530

    Added Index page and Controller. Closes #8

---
+++ petclinicWeb/src/main/java/petclinicWeb/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package petclinicWeb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(
+    { "", "/" }
+)
+public class IndexController
+{
+	private final String indexPage = "index";
+	
+	@GetMapping(
+	    { "/", "", "index" }
+	)
+	public String loadIndex(
+	)
+	{
+		return indexPage;
+	}
+}
