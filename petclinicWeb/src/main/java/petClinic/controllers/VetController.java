dd40f4a6a3bb509532be7fa231e5412987f34c6e
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2020-10-05 16:05:35 +0530

    Closes #12

--- petclinicWeb/src/main/java/petClinic/controllers/VetController.java
+++ petclinicWeb/src/main/java/petClinic/controllers/VetController.java
@@ -1,8 +1,12 @@
 package petClinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import petClinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
@@ -10,11 +14,23 @@ public class VetController
 {
 	private final String vetList = "vets/list";
 	
-	@GetMapping("/list")
-	public String getVetsList(
+	private final VetService vetSrv;
+	
+	@Autowired
+	public VetController(
+	        VetService vetSrv
 	)
 	{
-		
+		super();
+		this.vetSrv = vetSrv;
+	}
+	
+	@GetMapping("/list")
+	public String getVetsList(
+	        Model model
+	)
+	{
+		model.addAttribute("vets", vetSrv.findAll());
 		return vetList;
 	}
 }
