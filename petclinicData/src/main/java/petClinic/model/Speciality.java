063b67e2f10ec67c562adfdda515463016228de3
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2020-10-08 12:34:34 +0530

    Closes #22

---
+++ petclinicData/src/main/java/petClinic/model/Speciality.java
@@ -0,0 +1,26 @@
+package petClinic.model;
+
+public class Speciality extends BaseEntity
+{
+	private String description;
+	
+	public String getDescription(
+	)
+	{
+		return description;
+	}
+	
+	public void setDescription(
+	        String description
+	)
+	{
+		this.description = description;
+	}
+	
+	public Speciality(
+	)
+	{
+		
+	}
+	
+}
--- petclinicData/src/main/java/petClinic/model/Vet.java
+++ petclinicData/src/main/java/petClinic/model/Vet.java
@@ -1,6 +1,29 @@
 package petClinic.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vet extends Person
 {
-
+	private List<Speciality> specialities;
+	
+	public List<Speciality> getSpecialities(
+	)
+	{
+		return specialities;
+	}
+	
+	public void setSpecialities(
+	        List<Speciality> specialities
+	)
+	{
+		this.specialities = specialities;
+	}
+	
+	public Vet(
+	)
+	{
+		this.specialities = new ArrayList<Speciality>();
+	}
+	
 }
