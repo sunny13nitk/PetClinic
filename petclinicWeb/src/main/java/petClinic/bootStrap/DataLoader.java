dec713557b22588c6293bc1270fd856107a40845
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2020-10-08 12:53:04 +0530

    Closes #27

--- petclinicWeb/src/main/java/petClinic/bootStrap/DataLoader.java
+++ petclinicWeb/src/main/java/petClinic/bootStrap/DataLoader.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import petClinic.model.Owner;
+import petClinic.model.PetType;
 import petClinic.model.Vet;
 import petClinic.services.OwnerService;
+import petClinic.services.PetTypeService;
 import petClinic.services.VetService;
 
 /*
@@ -17,17 +19,19 @@ import petClinic.services.VetService;
 public class DataLoader implements CommandLineRunner
 {
 	
-	private final OwnerService ownerSrv;
-	private final VetService   vetSrv;
+	private final OwnerService   ownerSrv;
+	private final VetService     vetSrv;
+	private final PetTypeService petTypeSrv;
 	
 	@Autowired
 	public DataLoader(
-	        OwnerService ownerSrv, VetService vetSrv
+	        OwnerService ownerSrv, VetService vetSrv, PetTypeService petTypeSrv
 	)
 	{
 		super();
-		this.ownerSrv = ownerSrv;
-		this.vetSrv   = vetSrv;
+		this.ownerSrv   = ownerSrv;
+		this.vetSrv     = vetSrv;
+		this.petTypeSrv = petTypeSrv;
 	}
 	
 	@Override
@@ -35,6 +39,15 @@ public class DataLoader implements CommandLineRunner
 	        String... args
 	) throws Exception
 	{
+		
+		PetType ptypeDog      = new PetType("dog");
+		PetType ptypeDogSaved = petTypeSrv.save(ptypeDog);
+		
+		PetType ptypeCat      = new PetType("cat");
+		PetType ptypeCatSaved = petTypeSrv.save(ptypeCat);
+		
+		System.out.println("Pet Types Loaded ....");
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Sunny");
 		owner1.setLastName("Bhardwaj");
