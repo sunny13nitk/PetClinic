5a33fa550eefa4ca1783b598b1a823540267ef4c
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2020-10-08 12:39:07 +0530

    closes #23.

--- petclinicData/src/main/java/petClinic/model/Owner.java
+++ petclinicData/src/main/java/petClinic/model/Owner.java
@@ -7,7 +7,51 @@ import java.util.List;
  */
 public class Owner extends Person
 {
+	
+	private String address;
+	private String city;
+	private String telephone;
+	
 	private List<Pet> pets;
+	
+	public String getAddress(
+	)
+	{
+		return address;
+	}
+	
+	public void setAddress(
+	        String address
+	)
+	{
+		this.address = address;
+	}
+	
+	public String getCity(
+	)
+	{
+		return city;
+	}
+	
+	public void setCity(
+	        String city
+	)
+	{
+		this.city = city;
+	}
+	
+	public String getTelephone(
+	)
+	{
+		return telephone;
+	}
+	
+	public void setTelephone(
+	        String telephone
+	)
+	{
+		this.telephone = telephone;
+	}
 	
 	public List<Pet> getPets(
 	)
